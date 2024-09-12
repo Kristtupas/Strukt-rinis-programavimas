519c5ba20c0dc4ef51a853d7ab2df036c6f9e7e2
Kristupas Girnius <s057713@ad.viko.lt>
2024-09-12 11:41:02 +0300

    prdejau pirma darba

---
+++ pirmas darbas/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string vardas ="Kristupas";
+    string pavarde ="Girnius";
+    int amzius= 18;
+    string grupe ="PI24";
+    int kursas = 1;
+    string studijuProgramosPavadinimas = "Programu sistemos";
+    cout << "Studento vardas: " <<vardas << endl;
+    cout << "Pavarde: " << pavarde << endl;
+    cout << "Amzius: " << amzius << endl;
+    cout << "Grupe: " << grupe << endl;
+    cout << "Kursas: " << kursas << endl;
+    cout << "Studiju,Programos,Pavadinimas: " << studijuProgramosPavadinimas << endl;
+    }

8617d1577a4d934a4c3cc02cf7f1b541a035321c
Kristtupas <kristupas2245@gmail.com>
2024-10-23 21:23:18 +0300

    Update main.cpp

--- praktinis2/main.cpp
+++ praktinis2/main.cpp
@@ -51,57 +51,6 @@ cin >> pasirinkimas;
 
 
 
-ar gali sujungti situos du kodus?
-
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-
-int main() 
-{
-    int pasirinkimas;
-
-do {
-cout << "\nPasirinkite veiksma:\n";
-cout << "1. Patikrinti ar raide yra balse\n";    
-cout << "2. Iseiti\n";
-cout << "Pasirinkite: ";
-cin >> pasirinkimas;
-
-    if (pasirinkimas == 1) {
-       char raide;
-       cout << "Iveskite raide: ";
-       cin >> raide;
-
-    // Patikriname ar raide yra balse
-    bool yraBalse = false;
-        
-     if (raide == 'a') {
-        yraBalse = true;
-    } else if ( raide == 'e') {
-        yraBalse = true;
-    } else if ( raide == 'i') {
-        yraBalse = true;
-    } else if ( raide == 'o') {
-        yraBalse = true;
-    } else if ( raide == 'u') {
-        yraBalse = true;
-    }
-
-        if (yraBalse) {
-            cout << "true" << endl;
-        } else {
-            cout << "false" << endl;
-        }
-    
-    }
-    
-} while (pasirinkimas != 2); 
-    
-  cout << "Programa baigta.\n";
-  return 0;
-}
 
 
 #include <iostream>  

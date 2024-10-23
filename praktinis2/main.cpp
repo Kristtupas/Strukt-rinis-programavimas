48298bb4c99a703d593a12eeb99d74e0c7928d99
Kristtupas <kristupas2245@gmail.com>
2024-10-23 21:22:23 +0300

    Update main.cpp

--- praktinis2/main.cpp
+++ praktinis2/main.cpp
@@ -3,14 +3,130 @@
 #include <ctime>
 using namespace std;
 
-//Funkcija tikrina ar raide yra balse
-bool yraBalse(char raide) {
-    char balsiai[] = {'a','e','i','o','u' };
-    for(int i=0; i<10; i++) {
-        if (raide==balsiai[i]) {
-            return true;
-        }
+int main() 
+{
+    int pasirinkimas;
+
+do {
+cout << "\nPasirinkite veiksma:\n";
+cout << "1. Patikrinti ar raide yra balse\n";    
+cout << "2. Iseiti\n";
+cout << "Pasirinkite: ";
+cin >> pasirinkimas;
+
+    if (pasirinkimas == 1) {
+       char raide;
+       cout << "Iveskite raide: ";
+       cin >> raide;
+
+    // Patikriname ar raide yra balse
+    bool yraBalse = false;
+        
+     if (raide == 'a') {
+        yraBalse = true;
+    } else if ( raide == 'e') {
+        yraBalse = true;
+    } else if ( raide == 'i') {
+        yraBalse = true;
+    } else if ( raide == 'o') {
+        yraBalse = true;
+    } else if ( raide == 'u') {
+        yraBalse = true;
     }
-return false;
+
+        if (yraBalse) {
+            cout << "true" << endl;
+        } else {
+            cout << "false" << endl;
+        }
+    
+    }
+    
+} while (pasirinkimas != 2); 
+    
+  cout << "Programa baigta.\n";
+  return 0;
 }
 
+
+
+
+ar gali sujungti situos du kodus?
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() 
+{
+    int pasirinkimas;
+
+do {
+cout << "\nPasirinkite veiksma:\n";
+cout << "1. Patikrinti ar raide yra balse\n";    
+cout << "2. Iseiti\n";
+cout << "Pasirinkite: ";
+cin >> pasirinkimas;
+
+    if (pasirinkimas == 1) {
+       char raide;
+       cout << "Iveskite raide: ";
+       cin >> raide;
+
+    // Patikriname ar raide yra balse
+    bool yraBalse = false;
+        
+     if (raide == 'a') {
+        yraBalse = true;
+    } else if ( raide == 'e') {
+        yraBalse = true;
+    } else if ( raide == 'i') {
+        yraBalse = true;
+    } else if ( raide == 'o') {
+        yraBalse = true;
+    } else if ( raide == 'u') {
+        yraBalse = true;
+    }
+
+        if (yraBalse) {
+            cout << "true" << endl;
+        } else {
+            cout << "false" << endl;
+        }
+    
+    }
+    
+} while (pasirinkimas != 2); 
+    
+  cout << "Programa baigta.\n";
+  return 0;
+}
+
+
+#include <iostream>  
+
+using namespace std;
+
+int didziausiasBendrasDaliklis(int a, int b) {
+    while (b != 0) {  
+        int laikinai = b;  
+        b = a % b;  
+        a = laikinai;  
+    }
+    return a; 
+}
+
+int main() {
+    int sk1, sk2;  
+
+    cout << "Įveskite pirmą skaičių: ";
+    cin >> sk1; 
+    cout << "Įveskite antrą skaičių: ";
+    cin >> sk2; 
+
+    cout << "Didžiausias bendras daliklis: " 
+         << didziausiasBendrasDaliklis(sk1, sk2) << endl;
+
+    return 0;  
+}
